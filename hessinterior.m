df76cf36c9bede1e170923ea473aa86411ff42d3
Anirudh Chari <anirudh.chari05@gmail.com>
2022-09-06 08:31:05 -0500

    use sparse matrices, can test speedup later

--- hessinterior.m
+++ hessinterior.m
@@ -1,18 +1,18 @@
 function h = hessinterior(w, lambda, n, V)
     hessf = zeros(n);
-    hessc = zeros([n, n, n*(n-1)/2]);
+    h = hessf;
     idx = 1;
     for i = 1:n
         for j = (i+1):n
-            hessc(i, i, idx) = -2*V(i, 1)^2 - 2*V(i, 2)^2 - 2*V(i, 3)^2;
-            hessc(i, j, idx) = 2*V(i, 1)*V(j, 1) + 2*V(i, 2)*V(j, 2) + 2*V(i, 3)*V(j, 3);
-            hessc(j, i, idx) = 2*V(i, 1)*V(j, 1) + 2*V(i, 2)*V(j, 2) + 2*V(i, 3)*V(j, 3);
-            hessc(j, j, idx) = -2*V(j, 1)^2 - 2*V(j, 2)^2 - 2*V(j, 3)^2;
+            hessc = zeros(n);
+            hessc(i, i) = -2*V(i, 1)^2 - 2*V(i, 2)^2 - 2*V(i, 3)^2;
+            hessc(i, j) = 2*V(i, 1)*V(j, 1) + 2*V(i, 2)*V(j, 2) + 2*V(i, 3)*V(j, 3);
+            hessc(j, i) = 2*V(i, 1)*V(j, 1) + 2*V(i, 2)*V(j, 2) + 2*V(i, 3)*V(j, 3);
+            hessc(j, j) = -2*V(j, 1)^2 - 2*V(j, 2)^2 - 2*V(j, 3)^2;
+            hesscsp = sparse(hessc);
+            h = h + lambda.ineqnonlin(idx) * hesscsp;
+%             h = h + lambda.ineqnonlin(idx) * hessc;
             idx = idx + 1;
         end
-    end
-    h = hessf;
-    for i = 1:n*(n-1)/2
-        h = h + lambda.ineqnonlin(i) * hessc(:, :, i);
     end
 end
